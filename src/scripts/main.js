6d5a974196eaffb91868fced92c5cf8c8eb24266
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-05-16 21:48:51 +0300

    smal changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,17 @@ let total = 0;
 let average = 0;
 
 const numbers = [...document.querySelectorAll('span.population')];
-const population = numbers.map((item) => item.textContent.replaceAll(',', ''));
+const population = numbers.map((item) => {
+  const parsed = Number(item.textContent.replace(/,/g, ''));
 
-for (const number of population) {
-  total += Number(number);
-}
+  return isNaN(parsed) ? 0 : parsed;
+});
 
-total.toLocaleString('en-US');
-average = total / population.length;
+total = population.reduce((acc, num) => acc + num, 0);
+average = population.length ? (total / population.length).toFixed(2) : '0.00';
 
-document.querySelector('span.total-population').textContent = total;
-document.querySelector('span.average-population').textContent = average;
+const totalFormat = total.toLocaleString('en-US');
+const averageFormat = Number(average).toLocaleString('en-US');
+
+document.querySelector('span.total-population').textContent = totalFormat;
+document.querySelector('span.average-population').textContent = averageFormat;
