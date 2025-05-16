73a0acb67a0b86e529faca3dee84932efb4affef
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-05-16 22:12:13 +0300

    zeroes were changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,7 +14,10 @@ total = population.reduce((acc, num) => acc + num, 0);
 average = population.length ? (total / population.length).toFixed(2) : '0.00';
 
 const totalFormat = total.toLocaleString('en-US');
-const averageFormat = Number(average).toLocaleString('en-US');
+const averageFormat = Number(average).toLocaleString('en-US', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
 
 document.querySelector('span.total-population').textContent = totalFormat;
 document.querySelector('span.average-population').textContent = averageFormat;
